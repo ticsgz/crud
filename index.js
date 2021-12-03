02f9cca852fbb476c3e651a24fb8a2b2edf186de
ticsgz <tics@grupozurita.com.ec>
2021-12-03 10:17:16 -0500

    conecting to mongo atlas

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict";
-const express = require('express');
+const express = require("express");
 var app = express();
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/views/index.html');
+var mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, { useUnifiedTopology: true });
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/client/views/index.html");
+});
+app.get("/api/hello/", (req, res) => {
+    res.json({ hello: "hello worldd" });
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('listening on port ' + port);
+    console.log("listening on port " + port);
 });
+//
--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
-const express = require('express')
-var app = express()
+const express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+require("dotenv").config()
 
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true }
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/views/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/views/index.html");
+});
 
-const port = process.env.PORT || 3000
+app.get("/api/hello/", (req, res) => {
+  res.json({ hello: "hello worldd" });
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('listening on port ' + port);
-    
-})
+  console.log("listening on port " + port);
+});
+//
