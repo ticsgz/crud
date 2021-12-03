28c12e90b4bc4bd108822a7953dec59eb24a74b6
ticsgz <tics@grupozurita.com.ec>
2021-12-03 09:56:46 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require('express');
+var app = express();
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/views/index.html');
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
